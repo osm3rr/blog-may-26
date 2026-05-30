d530a3fddbd9609322da9a63b8b7cf0c54696b6b
osm3rr <osm3rr@aakademy.com>
2026-05-30 17:07:51 -0400

    update is done

--- newspapers/urls.py
+++ newspapers/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import BlogListView, BlogDetailView, BlogCreateView
+from .views import (
+                    BlogListView,
+                    BlogDetailView,
+                    BlogCreateView,
+                    BlogUpdateView,    
+                )
 
 urlpatterns = [
     path('', BlogListView.as_view(), name='post-list'),
     path('post/<int:pk>/', BlogDetailView.as_view(), name='post-detail'),
     path('post/crear/', BlogCreateView.as_view(), name='post-create'),
+    path('post/actualizar/<int:pk>/', BlogUpdateView.as_view(), name='post-update'),
 ]
--- newspapers/views.py
+++ newspapers/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from .models import Blog
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import (
+                                    ListView,
+                                    DetailView,
+                                    CreateView,
+                                    UpdateView,
+                                )
 
 # Create your views here. 
 class BlogListView(ListView):
@@ -16,4 +21,7 @@ class BlogCreateView(CreateView):
     template_name = 'post-create.html'
     fields = ['title', 'content', 'author']
     
-    
+class BlogUpdateView(UpdateView):
+    model = Blog
+    template_name = 'post-update.html'
+    fields = ['title', 'content']
